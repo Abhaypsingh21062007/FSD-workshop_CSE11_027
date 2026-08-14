891d3b8c4963ec35fbe4445109ddab9d7232b886
Abhay singh <abhaypsingh21607@gmail.com>
2026-08-14 14:41:47 +0530

    Lab 1 completed

---
+++ Backend/Lab1/index.js
@@ -0,0 +1,15 @@
+import EventEmmiter from "node:events";
+const myEmmiter = new EventEmmiter();
+
+myEmmiter.on("greet", (student) => {         
+console.log(`class started by ${student}`);
+});
+
+myEmmiter.on("exit", (student) => {
+
+console.log(`class finished by ${student}`);
+
+});
+myEmmiter.emit("greet", "Abhay");
+myEmmiter.emit("exit", "Abhay");
+

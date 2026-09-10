1c564d06303dca090362e396e75c5d08197a9fd0
Abhay singh <abhaypsingh21607@gmail.com>
2026-09-10 09:00:13 +0530

    Assignment 3

--- Backend/Lab3/http-server1.js
+++ Backend/Lab3/http-server1.js
@@ -1,11 +1,12 @@
 import http from "http";
 import fs from "node:fs/promises";
 
-const port = 3001;
+const port = 3000;
 const filePath = "file.txt";
 
-let messages = [];
+let users = [];
 
+// Read file
 async function readFile() {
     try {
         const data = await fs.readFile(filePath, "utf-8");
@@ -16,17 +17,20 @@ async function readFile() {
     }
 }
 
+// Read file content
 const content = await readFile();
 
 console.log(content);
 
+// Create server
 const server = http.createServer((req, resp) => {
 
     const url = req.url;
-    const typ = req.method;
+    const method = req.method;
+
 
     // GET /msg
-    if (url === "/msg" && typ === "GET") {
+    if (url === "/msg" && method === "GET") {
 
         resp.writeHead(200, {
             "Content-Type": "application/json"
@@ -39,11 +43,12 @@ const server = http.createServer((req, resp) => {
 
 
     // GET /sis
-    else if (url === "/sis" && typ === "GET") {
+    else if (url === "/sis" && method === "GET") {
 
         const userData = {
-            name: "abhay",
-            id: 1223
+            name: "abhi",
+            id: 12,
+            class: "B.Tech"
         };
 
         resp.writeHead(200, {
@@ -55,14 +60,16 @@ const server = http.createServer((req, resp) => {
 
 
     // POST /create
-    else if (url === "/create" && typ === "POST") {
+    else if (url === "/create" && method === "POST") {
 
         let body = "";
 
-        req.on("data", chunk => {
+        // Receive data
+        req.on("data", (chunk) => {
             body += chunk.toString();
         });
 
+        // When data receiving is complete
         req.on("end", () => {
 
             try {
@@ -71,10 +78,12 @@ const server = http.createServer((req, resp) => {
 
                 const newUser = {
                     id: data.id,
-                    name: data.name
+                    name: data.name,
+                    class: data.class
                 };
 
-                messages.push(newUser);
+                // Store user in array
+                users.push(newUser);
 
                 resp.writeHead(201, {
                     "Content-Type": "application/json"
@@ -99,7 +108,116 @@ const server = http.createServer((req, resp) => {
     }
 
 
-    // Unknown Route
+    // GET /users
+    else if (url === "/users" && method === "GET") {
+
+        resp.writeHead(200, {
+            "Content-Type": "application/json"
+        });
+
+        resp.end(JSON.stringify(users));
+    }
+
+
+    // PUT /edit/:id
+    else if (url.startsWith("/edit/") && method === "PUT") {
+
+        const id = parseInt(url.split("/")[2]);
+
+        let body = "";
+
+        // Receive data
+        req.on("data", (chunk) => {
+            body += chunk.toString();
+        });
+
+        // When data receiving is complete
+        req.on("end", () => {
+
+            try {
+
+                const data = JSON.parse(body);
+
+                // Find user
+                const index = users.findIndex(user => user.id === id);
+
+                // User not found
+                if (index === -1) {
+
+                    resp.writeHead(404, {
+                        "Content-Type": "application/json"
+                    });
+
+                    resp.end(JSON.stringify({
+                        message: "User not found"
+                    }));
+
+                    return;
+                }
+
+                // Update user
+                users[index] = {
+                    id: id,
+                    name: data.name,
+                    class: data.class
+                };
+
+                resp.writeHead(200, {
+                    "Content-Type": "application/json"
+                });
+
+                resp.end(JSON.stringify({
+                    message: "User updated successfully",
+                    user: users[index]
+                }));
+
+            } catch (error) {
+
+                resp.writeHead(400, {
+                    "Content-Type": "application/json"
+                });
+
+                resp.end(JSON.stringify({
+                    message: "Invalid JSON data"
+                }));
+            }
+        });
+    }
+
+
+    // DELETE /delete/:id
+    else if (url.startsWith("/delete/") && method === "DELETE") {
+
+        const id = parseInt(url.split("/")[2]);
+
+        const index = users.findIndex(user => user.id === id);
+
+        if (index !== -1) {
+
+            users.splice(index, 1);
+
+            resp.writeHead(200, {
+                "Content-Type": "application/json"
+            });
+
+            resp.end(JSON.stringify({
+                message: "User deleted successfully"
+            }));
+
+        } else {
+
+            resp.writeHead(404, {
+                "Content-Type": "application/json"
+            });
+
+            resp.end(JSON.stringify({
+                message: "User not found"
+            }));
+        }
+    }
+
+
+    // Unknown route
     else {
 
         resp.writeHead(404, {
@@ -113,6 +231,7 @@ const server = http.createServer((req, resp) => {
 });
 
 
+// Start server
 server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
